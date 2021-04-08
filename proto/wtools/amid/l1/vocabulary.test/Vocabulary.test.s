dec3b15f3c4dda44a2c78f070bc593daf1b8eab1
wandalen <wandalen.me@gmail.com>
2021-04-08 15:34:32 +0300

    .

--- proto/wtools/amid/l1/vocabulary.test/Vocabulary.test.s
+++ proto/wtools/amid/l1/vocabulary.test/Vocabulary.test.s
@@ -5,7 +5,7 @@
 
 if( typeof module !== 'undefined' )
 {
-  let _ = require( '../../../../node_modules/Tools' );
+  const _ = require( '../../../../node_modules/Tools' );
   require( '../vocabulary/Vocabulary.s' );
   _.include( 'wTesting' );
 }
